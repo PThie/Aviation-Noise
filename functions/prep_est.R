abedba5eeb7909a534f527fe552d397863c6d902
Thiel <pthiel@rwi-essen.de>
2023-01-16 10:00:50 +0100

    adjusted prep est

--- functions/prep_est.R
+++ functions/prep_est.R
@@ -1,45 +1,47 @@
-pacman::p_load(roxygen2)
-
-
 prep_est <- function(housing_data){
-  ##### documentation
-  #' Preparation estimation
-  #' 
-  #' \code{prep_est} prepares the data for estimation
-  #' 
-  #' @param takes housing data as input (after the contour rings indicators have
-  #'   been added)
-  #'   
-  #' @return Returns housing data ready for estimation
-  #' 
-  #' @export
-  
-  
-  ##### function itself
-  # make "months" factor variable
-  housing_data$months <- as.factor(housing_data$year_mon)
-  
-  # drop geometry
-  housing_data <- st_drop_geometry(housing_data)
-  
-  # restrict to 5km to contour ring
-  housing_data <- housing_data %>% filter(distance_main_airports <= 5)
-  
-  # exlcude 1km buffer
-  housing_data <- housing_data %>% filter(distance_main_airports >= 1 | distance_main_airports == 0)
-  
-  # drop March 2020
-  housing_data <- housing_data %>% filter(year_mon != "2020-03")
+    #' @title Preparation for estimation
+    #' 
+    #' @description This function prepares the data for estimation.
+    #' 
+    #' @param housing_data Housing data after contour information and additonal
+    #' variables have been added
+    #'   
+    #' @return Returns housing data ready for estimation
+    #' 
+    #' @author Patrick Thiel
+    
+    #----------------------------------------------
+    # drop geometry
+    housing_data <- st_drop_geometry(housing_data)
 
-  # drop Airports Tegel and Schoenefeld
-  housing_data <- housing_data %>% filter(closest_main_airports != "EDDT" & closest_main_airports != "EDDB")
-  
-  # add ring for 60dB and above
-  housing_data <- housing_data %>% mutate(con_ring8 = case_when(con_ring1 == 1 | con_ring2 == 1 | con_ring3 == 1 | con_ring4 == 1 ~ 1,
-                                          con_ring5 == 1 ~ 0))
+    # make "months" factor variable
+    housing_data$months <- as.factor(housing_data$year_mon_end)
+    
+    # restrict sample
+    housing_data <- housing_data |>
+        # restrict to 5km to contour ring
+        filter(distance_main_airports <= 5) |>
+        # exlcude 1km buffer
+        filter(distance_main_airports >= 1 | distance_main_airports == 0) |>
+        # drop March 2020
+        filter(year_mon_end != "2020-03") |>
+        # drop Airports Tegel and Schoenefeld
+        filter(closest_main_airports != "EDDT" & closest_main_airports != "EDDB")
+    
+    # add ring for 60dB and above
+    housing_data <- housing_data |>
+        mutate(
+            con_ring8 = case_when(
+                con_ring1 == 1 |
+                con_ring2 == 1 |
+                con_ring3 == 1 |
+                con_ring4 == 1 ~ 1,
+                con_ring5 == 1 ~ 0
+            )
+        )
 
-  housing_data$con_ring8[is.na(housing_data$con_ring8)] <- 0
+    housing_data$con_ring8[is.na(housing_data$con_ring8)] <- 0
 
-  # return
-  return(housing_data)
+    # return
+    return(housing_data)
 }
